cf0040e0fa52798d33721d91d7fc2ff01a7f8ee9
hcc <827773271@qq.com>
2021-01-13 01:24:17 +0800

    add anyproxy_rule.js script and websocket

---
+++ node_proj/src/CCWebSocket.js
@@ -0,0 +1,111 @@
+const WebSocket = require('ws');
+const os = require("os")
+
+console.log("hcc>> websocket Class: ", WebSocket)
+
+function isLinux() {
+    return os.platform() == "linux"
+}
+
+var web_socket_ip = isLinux() ? "121.41.0.245" : "127.0.0.1";
+var web_socket_port = 8005;
+var client_socket_list = {};
+
+function get_client_socket_key(client_socket) {
+    if(!client_socket_list){
+        return;
+    }
+
+    if (!client_socket._socket) {
+        return;
+    }
+    var client_key = client_socket._socket.remoteAddress + ":" + client_socket._socket.remotePort;
+    return client_key
+}
+
+function start_websocket_server() {
+    var server_web_socket = new WebSocket.Server({
+        host: web_socket_ip,
+        port: web_socket_port
+    });
+
+    server_web_socket.on("connection", function (client_socket) {
+       console.log("hcc>>client connection: ", client_socket._socket.remoteAddress + ":" + client_socket._socket.remotePort);
+       var client_key = get_client_socket_key(client_socket)
+       if (!client_key){
+           console.error("hcc>>client socket key is null!!");
+            return;
+       }
+        client_socket_list[client_key] = client_socket;
+       ws_add_client_session_event(client_socket)
+        console.log("hcc>>cur client count :", GetArrayLen(client_socket_list));
+    });
+
+    server_web_socket.on("listening", function () {
+        console.log("hcc>>WebSocket server start listen", ",ip:", web_socket_ip, " ,port:", web_socket_port);
+    });
+
+    server_web_socket.on("error", function (err) {
+        console.log("hcc>>WebSocket server listen error");
+    });
+
+    server_web_socket.on("close", function (err) {
+        console.log("hcc>>WebSocket server listen close!!");
+    });
+}
+
+//send data to all client
+function send_message_to_client(data_str) {
+    for(let client_ley in client_socket_list){
+        if (client_socket_list[client_ley].send) {
+            client_socket_list[client_ley].send(data_str)
+        }
+    }
+}
+
+//websocket 客户端session事件
+function ws_add_client_session_event(client_socket) {
+    client_socket.on("close", function () {
+        var client_key = get_client_socket_key(client_socket)
+        if (client_key && client_socket_list[client_key]) {
+            delete client_socket_list[client_key];
+        }
+        if (client_socket.close) {
+            client_socket.close();
+        }
+
+        console.log("hcc>>client closed:", client_key, " ,cur client count: ", GetArrayLen(client_socket_list));
+    });
+
+    client_socket.on("error", function (err) {
+    });
+
+    client_socket.on("message", function (data) {
+        if(data){
+            client_socket.send(data);
+            console.log("recv client message:");
+            console.log(data);
+        }
+    });
+}
+
+//获取对象{},或者数组[],的长度
+function GetArrayLen(array) {
+    if (!array) {
+        return 0;
+    }
+    let count = 0;
+    for (const key in array) {
+        count++;
+    }
+    return count;
+}
+
+var CCWebSocket = {
+    start_websocket_server: start_websocket_server,
+    send_message_to_client: send_message_to_client,
+}
+
+module.exports = CCWebSocket;
+//test
+// start_websocket_server();
---
+++ node_proj/src/anyproxy_rule.js
@@ -0,0 +1,152 @@
+
+var CCWebSocket = require("./CCWebSocket.js")
+// console.log("CCCCCCC",CCWebSocket)
+CCWebSocket.start_websocket_server();
+
+module.exports = {
+  // 模块介绍
+  summary: 'hcc>> anyproxy_rule.js',
+   /*
+    //requestDetail
+    protocol {string} 请求使用的协议，http或者https
+    requestOptions {object} 即将发送的请求配置，供require('http').request作为使用。详见：https://nodejs.org/api/http.html#http_http_request_options_callback
+    requestData {object} 请求Body
+    url {string} 请求url
+    _req {object} 请求的原始request
+    例：
+    {
+      protocol: 'http',
+      url: 'http://anyproxy.io/',
+      requestOptions: {
+        hostname: 'anyproxy.io',
+        port: 80,
+        path: '/',
+        method: 'GET',
+        headers: {
+          Host: 'anyproxy.io',
+          'Proxy-Connection': 'keep-alive',
+          'User-Agent': '...'
+        }
+      },
+      requestData: '...',
+      _req: {}
+    }
+  */
+  // 发送请求前拦截处理
+  // 如果beforeSendRequest返回了响应内容，则立即把此响应返回到客户端（而不再发送到真正的服务端），流程结束。
+  *beforeSendRequest(requestDetail) {
+     // const newRequestOptions = requestDetail.requestOptions;
+     // 设置属性 rejectUnauthorized = true: 进行证书的强验证
+      // newRequestOptions.rejectUnauthorized = false; //证书出错，不进行强制验证
+      // return {
+      //   requestOptions: newRequestOptions
+      // };
+    // 不做任何处理，返回null
+    return null;
+   },
+
+  
+  /*
+    responseDetail
+    requestDetail 同beforeSendRequest中的参数
+    responseDetail
+    response {object} 服务端的返回信息，包括statusCode header body三个字段
+    _res {object} 原始的服务端返回对象
+    举例，请求 anyproxy.io 时，responseDetail参数内容大致如下
+    {
+      response: {
+        statusCode: 200,
+        header: {
+          'Content-Type': 'image/gif',
+          Connection: 'close',
+          'Cache-Control': '...'
+        },
+        body: '...'
+      },
+      _res: { }
+    }
+   */
+  // 发送响应前处理
+  *beforeSendResponse(requestDetail, responseDetail) {
+    
+    var request_host = requestDetail.requestOptions.hostname || "empty";
+    var request_url = requestDetail.url || "empty";
+    var request_method = requestDetail.requestOptions.method || "empty";
+    var request_headers = requestDetail.requestOptions.headers || "empty";
+    var request_data = requestDetail.requestData || "empty";
+    /*
+    console.log("request---------------------start\n");
+    console.log("hcc>>reqHost: " + request_host);
+    console.log("hcc>>reqUrl: " + request_url);
+    console.log("hcc>>method: " + request_method);
+    console.log("hcc>>headers: ");
+    console.log(request_headers);
+    console.log("hcc>>requestData: ");
+    console.log(request_data);
+    console.log("request---------------------end\n");
+
+    console.log("\nresponse---------------------start\n");
+    var res_status_code = responseDetail.response.statusCode
+    var res_headers = responseDetail.response.header
+    var res_data_body = responseDetail.response.body
+    var res_raw_body = responseDetail.response.rawBody
+
+    console.log("hcc>>res_status_code: ")
+    console.log(res_status_code)
+    console.log("hcc>>resHeader: ")
+    console.log(res_headers)
+    console.log("hcc>>res_data_body: ")
+    console.log(res_data_body)
+    console.log("hcc>>res_raw_body: ")
+    console.log(res_raw_body)
+    console.log("\nresponse---------------------end\n");
+    */
+
+    var req_headers_str = ""
+    for(let key in request_headers){
+        req_headers_str = req_headers_str + key + ": " + request_headers[key] + "\n"
+    }
+
+    // data send to client 
+    var request_message = "[reqHeader<" + request_host + ">]\n"
+    request_message = request_message + "ReqUrl: " + request_url + "\n"
+    request_message = request_message + "Method: " + request_method + "\n"
+    request_message = request_message + req_headers_str
+    
+    console.log(request_message)
+    CCWebSocket.send_message_to_client(request_message)
+
+   /*
+   console.log("hcc>>请求:>>>>>start>>\n");
+   console.log(requestDetail);
+   console.log("hcc>>请求:>>>>>end>>\n");
+
+   console.log("hcc>>返回:>>>>>start>>\n");
+   console.log(responseDetail);
+   console.log("hcc>>返回:>>>>>end>>\n");
+   */
+    // 不做任何处理，返回null
+    return null;
+  },
+
+  /*
+    requestDetail
+    host {string} 请求目标的Host，受制于协议，这里无法获取完整url
+    _req {object} 请求的原始request
+  */
+  // 是否处理https请求
+  // beforeDealHttpsRequest方法，如果返回true，会明文解析这个请求，其他请求不处理
+  // 如果配置了全局解析https的参数，则AnyProxy会略过这个调用
+  *beforeDealHttpsRequest(requestDetail) {
+    return true
+  },
+
+  // 请求出错的事件
+  *onError(requestDetail, error) {
+
+  },
+  // https连接服务器出错
+  *onConnectError(requestDetail, error) {
+
+  }
+};
