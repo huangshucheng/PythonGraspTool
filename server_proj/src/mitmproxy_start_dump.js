c56ded33973bca3eb99516e49dffee9550112112
hcc <827773271@qq.com>
2021-01-12 10:42:55 +0800

    add -q commond

--- server_proj/src/mitmproxy_start_dump.js
+++ server_proj/src/mitmproxy_start_dump.js
@@ -8,7 +8,7 @@ console.log("hcc>>path>>dump>> " + script_path);
 // var start_str = "mitmweb -p 8001 --web-host 0.0.0.0 --web-port 8002 --set block_global=false --no-web-open-browser -s " + script_path
 
 //use dump
-var start_str = "mitmdump -p 8001 --set block_global=false -s " + script_path
+var start_str = "mitmdump -p 8001 --set block_global=false -q -s " + script_path
 
 exec(start_str,(error,stdout,stderr)=>{
     if(error){
